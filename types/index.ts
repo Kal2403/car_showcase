a05fb3002dbfc709271c5f68bf2a0a616f6e7306
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-13 01:00:41 +0200

    Initial commit New project

--- types/index.ts
+++
@@ -1,8 +0,0 @@
-import { MouseEventHandler } from "react";
-
-export interface CustomButtonProps {
-  title: string;
-  constainerStyles?: string;
-  handlleClick?: MouseEventHandler<HTMLButtonElement>;
-  btnType: "button" |
-}
